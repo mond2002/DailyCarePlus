5c3b7e7c5472781d7681f5943b17db433a8d6504
mond2002 <mond2006@naver.com>
2022-02-08 19:28:42 +0900

    0208_1928
    브랜치저장

--- app/src/main/java/com/example/dailycareplus/MainMenuActivity.kt
+++ app/src/main/java/com/example/dailycareplus/MainMenuActivity.kt
@@ -2,11 +2,11 @@ package com.example.dailycareplus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
-
     }
 }
